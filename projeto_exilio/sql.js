16cd3ffed4d887d1dab3c239a81fc41666a64cc0
Wesley Victor Ferreira <166853717+WesleyVictors@users.noreply.github.com>
2025-02-15 05:22:26 +0000

    mudança na api

--- projeto_exilio/sql.js
+++ projeto_exilio/sql.js
@@ -19,4 +19,11 @@ export const sql = postgres({
     options: `project=${ENDPOINT_ID}`,
   },
 });
+db.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados: ', err);
+    process.exit(1);
+  }
+  console.log('Conectado ao banco de dados MySQL!');
+});
 
